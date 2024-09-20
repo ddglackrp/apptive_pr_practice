55a65e791aab492ff6a2c0704d22fe0dfb2ed4c2
JJY <ddglackrp@gmail.com>
2024-09-20 19:14:10 +0900

    feat 'Implement multy method'

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def subtract(a, b):
     return a-b
 
 def multiply(a, b):
-    pass
+    return a*b
 
 
 def divide(a, b):
@@ -28,3 +28,4 @@ def mod(a, b):
 if __name__ == "__main__":
     print(add(1,3))
     print(subtract(1,2))
+    print(multiply(1,2))
