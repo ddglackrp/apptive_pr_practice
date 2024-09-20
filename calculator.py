0a6a0c498a8c70eaace7e5545c91bfd738912cdd
myeowon <myw7931@gmail.com>
2024-09-20 19:15:04 +0900

    commit

--- calculator.py
+++ calculator.py
@@ -1,31 +1,41 @@
 def add(a, b):
-    pass
+    print(a+b)
 
 
 def subtract(a, b):
-	pass
+	print( a-b )
 
 
 def multiply(a, b):
-    pass
+    print( a*b)
 
 
 def divide(a, b):
-    pass
+    print( a/b)
 
 
 def pow(a, b):
-    pass
+    print( a**b)
 
 
 def abs(a):
-    pass
+    if a > 0 :
+        print( a)
+    else :
+        print( -a)
 
 
 def mod(a, b):
-    pass
+    print( a%b)
+
 
 
 if __name__ == "__main__":
     # 간단한 테스트 코드
-    pass
+    add(1,2)
+    subtract(3,2)
+    multiply(3,4)
+    divide(8,2)
+    pow(9,3)
+    abs(5)
+    mod(8,3)
